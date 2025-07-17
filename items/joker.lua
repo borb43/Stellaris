cd7f686e4f1d82ad59b984cfefbfbbb9b49f5b06
Eris (borb) <remcosveen@gmail.com>
2025-07-17 21:55:37 +0200

    actually made a fucking mod fuck yeah

--- items/joker.lua
+++ items/joker.lua
@@ -7,9 +7,9 @@ SMODS.Joker {
             "contains a {C:attention}Full House{}"
         }
     },
-    config = { extra = { mult = 18 }},
+    config = { extra = { mult = 18, type = "Full House" } },
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult } }
+        return { vars = { card.ability.extra.mult, card.ability.extra.type } }
     end,
     discovered = true,
     rarity = 1,
@@ -20,6 +20,33 @@ SMODS.Joker {
         if context.joker_main and next(context.poker_hand[card.ability.extra.type]) then
             return {
                 mult = card.ability.extra.mult
+            }
+        end
+    end
+}
+
+SMODS.Joker {
+    key = "dubious",
+    loc_txt = {
+        name = "Dubious Joker",
+        text = {
+            "{C:chips}+#1#{} Chips if played poker hand",
+            "contains a {C:attention}Full House{}"
+        }
+    },
+    config = { extra = { chips = 140, type = "Full House" } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.chips, card.ability.extra.type } }
+    end,
+    discovered = true,
+    rarity = 1,
+    atlas = "placeholder",
+    pos = { x = 0, y = 0 },
+    cost = 4,
+    calculate = function(self, card, context)
+        if context.joker_main and next(context.poker_hand[card.ability.extra.type]) then
+            return {
+                chips = card.ability.extra.chips
             }
         end
     end
