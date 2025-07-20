8d53a364d450d1e6a5f18361f2cef74c7b8ebe4e
Eris (borb) <remcosveen@gmail.com>
2025-07-20 13:48:37 +0200

    finished up legendary and i hope it works

--- items/legendary.lua
+++ items/legendary.lua
@@ -9,5 +9,15 @@ SMODS.Joker {
     atlas = "placeholder",
     pos = { x = 3, y = 0 },
     soul_pos = { x = 4, y = 0 },
-    blueprint_compat = false
+    blueprint_compat = false,
+    cost = 20,
+    calculate = function(self, card, context)
+        if context.selling_self and not context.blueprint then
+            local ante_change = -G.GAME.round_resets.ante + card.ability.extra.set_ante
+            ease_ante(ante_change)
+            G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
+            G.GAME.round_resets.blind_ante = card.ability.extra.set_ante
+            return true
+        end
+    end
 }
