70b1704366755381615eccad7c35a43515d8f4a4
Eris (borb) <remcosveen@gmail.com>
2025-07-21 21:54:12 +0200

    removed unnecessary drawstep from shatter

--- items/spectral.lua
+++ items/spectral.lua
@@ -39,10 +39,5 @@ SMODS.Consumable {
     end,
     can_use = function(self, card)
         return G.hand and #G.hand.cards > 1
-    end,
-    draw = function(self, card, layer)
-        if ( layer == "card" or layer == "both" ) and card.sprite_facing == "front" then
-            card.children.center:draw_shader('booster', nil, card.ARGS.send_to_shader)
-        end
     end
 }
