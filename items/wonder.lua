cad543fc209d8b9fe7371f2523e826a24e468991
Eris (borb) <remcosveen@gmail.com>
2025-07-21 13:27:49 +0200

    added dream wheel, applies random edition to random joker

--- items/wonder.lua
+++ items/wonder.lua
@@ -6,3 +6,26 @@ SMODS.ConsumableType {
     default = "c_stlr_dreamwheel",
     shop_rate = 0.0,
 }
+
+SMODS.Consumable {
+    key = "dreamwheel",
+    set = "stlr_wonder",
+    atlas = "placeholder",
+    pos = { x = 3, y = 3 },
+    cost = 4,
+    use = function(self, card, area, copier)
+        local valid_targets = SMODS.Edition:get_edition_cards(G.jokers, true)
+        local editions = {}
+        for _, ed in pairs(G.P_CENTER_POOLS["Edition"]) do
+            if not G.GAME.banned_keys[ed.key] then
+                editions[#editions+1] = ed.key
+            end
+        end
+        local target = pseudorandom_element(valid_targets, "dreamwheel")
+        local target_edition = pseudorandom_element(editions, "dreamwheel")
+        target:set_edition(target_edition, true)
+    end,
+    can_use = function(self, card)
+        return next(SMODS.Edition:get_edition_cards(G.jokers, true))
+    end
+}
