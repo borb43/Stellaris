8d1e1b33c4dceaa3f1e92333d086c5b092c26bd2
Eris (borb) <remcosveen@gmail.com>
2025-07-21 19:19:42 +0200

    wow i fixed it

--- items/stellar.lua
+++ items/stellar.lua
@@ -1,6 +1,6 @@
 SMODS.Rarity {
     key = "stellar",
-    badge_color = HEX("1f0d35"),
+    badge_colour = HEX("1f0d35"),
     default_weight = 0,
     get_weight = function(self, weight, object_type)
         return weight
