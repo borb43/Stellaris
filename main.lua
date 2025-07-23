aa3428e0bd9108a24cc9985c8634954f5789b1a2
Eris (borb) <remcosveen@gmail.com>
2025-07-23 15:01:51 +0200

    added new colour

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 --for talisman compat
 to_big = to_big or function(x) return x end
 
-
-
 --for the HEX function, first 6 characters are RGB values, can add 2 more at the end for opacity
 --define and load custom text colours, then add into loc_colours
 G.C.PINK = HEX("eb90e8")
-G.ARGS.LOC_COLOURS["pink"] = G.C.PINK
+G.ARGS.LOC_COLOURS["pink"] = G.C.PINK --for pink seals
+G.C.CRIMSON = HEX("a52a2a")
+G.ARGS.LOC_COLOURS["crimson"] = G.C.CRIMSON --for blind modifying stuff
 
 --load optional features
 SMODS.current_mod.optional_features = {
