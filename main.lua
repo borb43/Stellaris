c3ce444cebb1a047b28a970f8765837eb4e42617
Eris (borb) <remcosveen@gmail.com>
2025-07-31 19:17:00 +0200

    improvements to enhancement banning hook

--- main.lua
+++ main.lua
@@ -7,16 +7,24 @@ to_big = to_big or function(x) return x end
 --make banning modifiers work with custom pools
 old_enhancement_roll = SMODS.poll_enhancement
 function SMODS.poll_enhancement(args)
-    if args.ignore_bans or args.options == nil then
-        return old_enhancement_roll(args)
-    else
+    if args.ignore_bans then
+        if not args.options then --allows ignoring bans with default pool
+            args.options = {}
+            for k, v in G.P_CENTER_POOLS.Enhanced do
+                args.options[#args.options + 1] = v
+            end
+            return old_enhancement_roll(args)
+        else
+            return old_enhancement_roll(args)
+        end
+    elseif args.options then --check if options was filled in, if so trim it to remove banned keys
         to_ban = {}
         for k, v in pairs(args.options) do
             if G.GAME.banned_keys[k] or G.GAME.banned_keys[v] then
-                to_ban[#to_ban+1] = k
+                to_ban[#to_ban + 1] = k
             elseif type(v) == "table" then
                 if G.GAME.banned_keys[v.key] then
-                    to_ban[#to_ban+1] = k
+                    to_ban[#to_ban + 1] = k
                 end
             end
         end
@@ -28,18 +36,20 @@ function SMODS.poll_enhancement(args)
         else
             return old_enhancement_roll(args)
         end
+    else --return default function if ignore_bans was false or nil and options wasnt filled in
+        return old_enhancement_roll(args)
     end
 end
 
 --for the HEX function, first 6 characters are RGB values, can add 2 more at the end for opacity
 --define and load custom text colours, then add into loc_colours
 G.C.PINK = HEX("eb90e8")
-G.ARGS.LOC_COLOURS["pink"] = G.C.PINK --for pink seals
+G.ARGS.LOC_COLOURS["pink"] = G.C.PINK       --for pink seals
 G.C.CRIMSON = HEX("a52a2a")
 G.ARGS.LOC_COLOURS["crimson"] = G.C.CRIMSON --for blind modifying stuff
 
 --load optional features (none yet)
 
 --loading stuff
-assert(SMODS.load_file("atlas.lua"))() --loads atlases
+assert(SMODS.load_file("atlas.lua"))()      --loads atlases
 assert(SMODS.load_file("fileloader.lua"))() --loads mod content files
