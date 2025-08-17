da1ba1690c23a330feedcb7fcf296b07ba5ab2be
Eris (borb) <remcosveen@gmail.com>
2025-08-17 19:16:53 +0200

    made some preparations for funny mechanic

---
+++ lib/corruptions.lua
@@ -0,0 +1,6 @@
+--void corruption info. table of uncorrupted and corrupted keys.
+--effectively also acts as plans for future corrupted variants
+
+Stellaris.corruptions = {
+    
+}
--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ loc_colour() --required for adding colours
 --for talisman compat
 to_big = to_big or function(x) return x end
 
+--mod globals table
+Stellaris = {}
 
 --for the HEX function, first 6 characters are RGB values, can add 2 more at the end for opacity
 --define and load custom text colours, then add into loc_colours
