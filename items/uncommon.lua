94c3027785bb5ae814d0c108803ddca1aaf0e358
Eris (borb) <remcosveen@gmail.com>
2025-08-26 18:15:02 +0200

    paint smear in_pool

--- items/uncommon.lua
+++ items/uncommon.lua
@@ -2,7 +2,7 @@ SMODS.Joker { --paint smear, gains x0.1 mult when a wild card is scored
     key = "paintsmear",
     config = { extra = { mult = 1, multgain = 0.1 } },
     loc_vars = function(self, info_queue, card)
-        info_queue[#info_queue+1] = G.P_CENTERS.m_wild
+        info_queue[#info_queue + 1] = G.P_CENTERS.m_wild
         return { vars = { card.ability.extra.mult, card.ability.extra.multgain } }
     end,
     discovered = true,
@@ -14,10 +14,10 @@ SMODS.Joker { --paint smear, gains x0.1 mult when a wild card is scored
     cost = 6,
     calculate = function(self, card, context)
         if context.individual and context.cardarea == G.play and not context.blueprint
-        and SMODS.has_enhancement(context.other_card, "m_wild") then
+            and SMODS.has_enhancement(context.other_card, "m_wild") then
             card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.multgain
             return {
-                message = localize { type = "variable", key = "a_xmult", vars = {card.ability.extra.mult} }
+                message = localize { type = "variable", key = "a_xmult", vars = { card.ability.extra.mult } }
             }
         end
         if context.joker_main then
@@ -25,5 +25,13 @@ SMODS.Joker { --paint smear, gains x0.1 mult when a wild card is scored
                 xmult = card.ability.extra.mult
             }
         end
+    end,
+    in_pool = function(self, args)
+        for _, playing_card in ipairs(G.playing_cards or {}) do
+            if SMODS.has_enhancement(playing_card, 'm_wild') then
+                return true
+            end
+        end
+        return false
     end
 }
