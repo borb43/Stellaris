79da42b22ff17e3c3ec51a0fb9192213af7165b1
Eris (borb) <remcosveen@gmail.com>
2025-08-30 11:31:54 +0200

    organization stuff

--- fileloader.lua
+++ fileloader.lua
@@ -3,10 +3,11 @@ assert(SMODS.load_file("lib/funcs.lua"))() --loads functions
 assert(SMODS.load_file("lib/game_globals.lua"))() --game globals resetting stuff
 assert(SMODS.load_file("lib/pos_blinds.lua"))()
 --jokers
-assert(SMODS.load_file("items/common.lua"))()
-assert(SMODS.load_file("items/uncommon.lua"))()
-assert(SMODS.load_file("items/rare.lua"))()
-assert(SMODS.load_file("items/legendary.lua"))()
+assert(SMODS.load_file("items/joker/common.lua"))()
+assert(SMODS.load_file("items/joker/uncommon.lua"))()
+assert(SMODS.load_file("items/joker/rare.lua"))()
+assert(SMODS.load_file("items/joker/legendary.lua"))()
+assert(SMODS.load_file("items/joker/stellar.lua"))
 --card modifiers, might split into seperate files in the future
 assert(SMODS.load_file("items/modifier.lua"))()
 --consumables
